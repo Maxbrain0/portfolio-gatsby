10d5bd8124cf6148961ff75d40d927e0e1bf20af
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-07-11 10:48:16 -0700

    Make sure layout is on all pages

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,10 @@
 import React from "react"
+import Layout from "../components/layout"
 
 export default () => (
-  <div>
-    <h1>Contact Me</h1>
-  </div>
+  <Layout>
+    <div>
+      <h1>Contact Me</h1>
+    </div>
+  </Layout>
 )
