e9f08ac7a9139c0358df2c13f4610f0f85436c43
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-07-14 22:02:01 -0700

    Add a favicon and pwa web app manifest support

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,5 +28,19 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `JG Professional`,
+        short_name: `JG Prof`,
+        start_url: `/`,
+        lang: `en`,
+        icon: `src/assets/icons/icon-512px-white.png`,
+        background_color: `#ffffff`,
+        theme_color: `#002d5b`,
+        display: `standalone`,
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
 }
