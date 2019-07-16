36d0f2aca84879b8ad71b63187f64ab1c04d24cf
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-07-16 11:24:04 -0700

    Update icon component to wrap svg in anchor tag

--- src/components/ui/icon.js
+++ src/components/ui/icon.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React from "react"
 
 // icon imports
 import BrandIcon from "../../assets/svg/brand.svg"
@@ -16,30 +16,41 @@ import TypeScriptIcon from "../../assets/svg/typescriptlang-icon.svg"
 import VueIcon from "../../assets/svg/vue.svg"
 import VuetifyIcon from "../../assets/svg/vuetify.svg"
 
-class Icon extends Component {
-  components = {
-    brand: BrandIcon,
-    bulma: BulmaIcon,
-    d3: D3Icon,
-    gatsby: GatsbyIcon,
-    ghost: GhostIcon,
-    handlebars: HandlebarsIcon,
-    npm: NpmIcon,
-    nuxt: NuxtIcon,
-    parcel: ParcelIcon,
-    react: ReactIcon,
-    sass: SassIcon,
-    typescript: TypeScriptIcon,
-    vue: VueIcon,
-    vuetify: VuetifyIcon,
+const Icon = props => {
+  const components = {
+    brand: { icon: BrandIcon },
+    bulma: { icon: BulmaIcon, url: "https://bulma.io/" },
+    d3: { icon: D3Icon, url: "https://d3js.org/" },
+    gatsby: { icon: GatsbyIcon, url: "https://www.gatsbyjs.org/" },
+    ghost: { icon: GhostIcon, url: "https://ghost.org/" },
+    handlebars: { icon: HandlebarsIcon, url: "https://handlebarsjs.com/" },
+    npm: { icon: NpmIcon, url: "https://www.npmjs.com/" },
+    nuxt: { icon: NuxtIcon, url: "https://nuxtjs.org/" },
+    parcel: { icon: ParcelIcon, url: "https://parceljs.org/" },
+    react: { icon: ReactIcon, url: "https://reactjs.org/" },
+    sass: { icon: SassIcon, url: "https://sass-lang.com/" },
+    typescript: {
+      icon: TypeScriptIcon,
+      url: "https://www.typescriptlang.org/",
+    },
+    vue: { icon: VueIcon, url: "https://vuejs.org/" },
+    vuetify: { icon: VuetifyIcon, url: "https://vuetifyjs.com/en/" },
   }
-  render() {
-    const IconName = this.components[this.props.type]
-    if (!IconName) {
-      return null
-    }
-    // get passed down classes
-    return <IconName className={this.props.className} />
+
+  const IconName = components[props.type].icon
+  if (!IconName) {
+    return null
   }
+  // get passed down classes
+  return (
+    <a
+      href={components[props.type].url}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <IconName className={props.className} />
+    </a>
+  )
 }
+
 export default Icon
