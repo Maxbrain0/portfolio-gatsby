ea30288835ccdc286dd7f0630fb057baaa31dea9
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-07-16 14:43:58 -0700

    Add more dummy content to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,31 @@ import Layout from "../components/layout"
 
 export default () => (
   <Layout>
-    <h1>This is the home page</h1>
-    <p>This is some generic content for the home page</p>
-    <h2>Subtitle</h2>
-    <p>This is sub content</p>
+    <h1>Welcome</h1>
+    <p>Centered Image here</p>
+
+    <p>
+      I'm Jacob Goodwin, a Web Developer experienced in writing Single Page
+      Applications. I'm have also worked with backend technologies such as Go,
+      Nodejs, gRPC, and GraphQL.
+    </p>
+
+    <h2>Currently working on</h2>
+    <p>Golang, gRPC, Flutter</p>
+
+    <h2>My story</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto maiores
+      voluptas est, magnam enim minus quod rem non corporis. Omnis alias quae,
+      nisi similique odit saepe fuga veniam facilis minus beatae quibusdam error
+      dignissimos, veritatis modi molestiae, suscipit repellat excepturi nulla
+      tempora! Consequuntur fuga ab nisi beatae est, soluta provident fugit
+      magnam pariatur vero, voluptatem sed iste sit, blanditiis repellendus
+      libero commodi quia aliquid dicta similique odit amet ratione! Hic
+      laboriosam architecto quaerat amet natus, vitae recusandae magnam aut,
+      error dignissimos minus eaque culpa consequuntur expedita possimus, eos
+      alias aliquid. Inventore iure est illum veritatis soluta odit harum fuga
+      rem.
+    </p>
   </Layout>
 )
