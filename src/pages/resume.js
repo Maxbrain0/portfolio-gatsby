81b3f287156b0876646910fcccfe7b7dd3424339
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-07-17 16:09:37 -0700

    Query data for resume page

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,15 +1,46 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import styles from "./resume.module.scss"
 
-export default () => (
-  <Layout>
-    <div className={styles.resume}>
-      <h1>Work Experience</h1>
+export default ({ data }) => {
+  console.log(data)
+  return (
+    <Layout>
+      <div className={styles.resume}>
+        <h1>Work Experience</h1>
 
-      <h1>Skills</h1>
+        <h1>Education</h1>
+      </div>
+    </Layout>
+  )
+}
 
-      <h1>Education</h1>
-    </div>
-  </Layout>
-)
+export const query = graphql`
+  query {
+    allEducationJson {
+      edges {
+        node {
+          school
+          dates
+          degree
+          other
+          thesis {
+            title
+            summary
+          }
+        }
+      }
+    }
+    allExperienceJson {
+      edges {
+        node {
+          title
+          company
+          dates
+          bullets
+        }
+      }
+    }
+  }
+`
