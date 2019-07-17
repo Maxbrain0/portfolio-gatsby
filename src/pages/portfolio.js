068e308b6184533f668149f4fcb5bcbef2d5bf08
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-07-16 18:21:59 -0700

    Combine ImageSharp into project data

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -5,15 +5,27 @@ import Layout from "../components/layout"
 import Project from "../components/project"
 
 export default ({ data }) => {
-  const projectData = data.allProjectsJson.edges
+  const projectData = data.allProjectsJson.edges.map(({ projectNode }) => {
+    const matchNode = data.allFile.edges.find(({ imgNode }) => {
+      return projectNode.imgName === imgNode.childImageSharp.fluid.originalName
+    })
 
-  const projects = projectData.map(({ node }) => (
-    <Project key={node.url} project={node} />
+    if (!matchNode) {
+      projectNode.img = null
+    } else {
+      projectNode.img = matchNode.imgNode.childImageSharp.fluid
+    }
+
+    return projectNode
+  })
+
+  const projects = projectData.map(project => (
+    <Project key={project.url} project={project} />
   ))
 
   return (
     <Layout>
-      <h1>Projects</h1>
+      <h1>Portfolio</h1>
       {projects}
     </Layout>
   )
@@ -23,12 +35,25 @@ export const query = graphql`
   query {
     allProjectsJson {
       edges {
-        node {
+        projectNode: node {
           title
           description
+          imgName
           url
           github
           icons
+        }
+      }
+    }
+    allFile(filter: { relativeDirectory: { eq: "projects" } }) {
+      edges {
+        imgNode: node {
+          childImageSharp {
+            fluid(maxWidth: 800) {
+              ...GatsbyImageSharpFluid
+              originalName
+            }
+          }
         }
       }
     }
