ba5e29402588b51e702391d4ad8f650c96898b41
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-07-16 21:16:00 -0700

    Start adding to resume section

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,10 +1,15 @@
 import React from "react"
 import Layout from "../components/layout"
+import styles from "./resume.module.scss"
 
 export default () => (
   <Layout>
-    <div>
-      <h1>Resume Page</h1>
+    <div className={styles.resume}>
+      <h1>Work Experience</h1>
+
+      <h1>Skills</h1>
+
+      <h1>Education</h1>
     </div>
   </Layout>
 )
