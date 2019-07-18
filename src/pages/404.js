ab71e1f815842692cc9bcea149d72f58b6be657f
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-07-18 12:50:04 -0700

    Update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,17 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import styles from "./404.module.scss"
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>This page doesn't exist</h1>
+    <p>But I do appreciate folks that like to meddle around. So kudos to ya!</p>
+
+    <Link to="/" className={styles.link}>
+      Back to Intro
+    </Link>
   </Layout>
 )
 
