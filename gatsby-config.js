38b5db1d7c2a3c0bd24f6bfedf89ad9d8a756d8c
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-07-18 08:19:33 -0700

    Install Helment plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-transformer-json`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
