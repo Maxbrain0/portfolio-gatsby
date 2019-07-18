783e5dc5665b4b6bf36a2af077e3f79665b50fb1
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-07-18 11:29:24 -0700

    Load home page info from json

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,11 +11,7 @@ export default ({ data }) => (
     <SEO title="Home" description="A professional introduction" />
     <h1>Welcome</h1>
 
-    <p>
-      I'm Jacob Goodwin, a Web Developer experienced in writing Single Page
-      Applications. I'm have also worked with backend technologies such as Go,
-      Nodejs, gRPC, and GraphQL.
-    </p>
+    <p>{data.home.intro}</p>
 
     <div className={styles.profileImage}>
       <Img
@@ -32,22 +28,10 @@ export default ({ data }) => (
     </div>
 
     <h2>Currently working on</h2>
-    <p>Golang, gRPC, Flutter</p>
+    <p>{data.home.current}</p>
 
     <h1>My story</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto maiores
-      voluptas est, magnam enim minus quod rem non corporis. Omnis alias quae,
-      nisi similique odit saepe fuga veniam facilis minus beatae quibusdam error
-      dignissimos, veritatis modi molestiae, suscipit repellat excepturi nulla
-      tempora! Consequuntur fuga ab nisi beatae est, soluta provident fugit
-      magnam pariatur vero, voluptatem sed iste sit, blanditiis repellendus
-      libero commodi quia aliquid dicta similique odit amet ratione! Hic
-      laboriosam architecto quaerat amet natus, vitae recusandae magnam aut,
-      error dignissimos minus eaque culpa consequuntur expedita possimus, eos
-      alias aliquid. Inventore iure est illum veritatis soluta odit harum fuga
-      rem.
-    </p>
+    <p>{data.home.about}</p>
   </Layout>
 )
 
@@ -61,6 +45,11 @@ export const query = graphql`
           ...GatsbyImageSharpFixed
         }
       }
+    }
+    home: dataJson {
+      intro
+      current
+      about
     }
   }
 `
