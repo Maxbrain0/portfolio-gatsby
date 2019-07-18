c7a663a5b7a7427f7cb592fc32e263dfd11e852c
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-07-18 11:12:37 -0700

    Fix typo of "query"

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -5,7 +5,7 @@ import { StaticQuery, graphql } from "gatsby"
 
 const SEO = ({ title, description, pathname, author }) => (
   <StaticQuery
-    quuery={query}
+    query={query}
     render={({
       site: {
         siteMetadata: {
@@ -25,30 +25,30 @@ const SEO = ({ title, description, pathname, author }) => (
       }
 
       return (
-        <Helmet title={seo.title} titleTemplate={titleTemplate}>
-          <link rel="canonical" href={siteUrl} />
-          <meta charSet="utf-8" />
-          <meta name="description" content={seo.description} />
-          {/* <meta name="image" content={seo.image} /> */}
-          {seo.url && <meta property="og:url" content={seo.url} />}
-          {seo.title && <meta property="og:title" content={seo.title} />}
-          {seo.description && (
-            <meta property="og:description" content={seo.description} />
-          )}
-          {/* {seo.image && <meta property="og:image" content={seo.image} />} */}
-          <meta name="twitter:card" content="summary_large_image" />
-          {seo.title && <meta name="twitter:title" content={seo.title} />}
-          {seo.description && (
-            <meta name="twitter:description" content={seo.description} />
-          )}
-          {/* {seo.image && <meta name="twitter:image" content={seo.image} />} */}
-        </Helmet>
+        <>
+          <Helmet title={seo.title} titleTemplate={titleTemplate}>
+            <link rel="canonical" href={siteUrl} />
+            <meta charSet="utf-8" />
+            <meta name="description" content={seo.description} />
+            {/* <meta name="image" content={seo.image} /> */}
+            {seo.url && <meta property="og:url" content={seo.url} />}
+            {seo.title && <meta property="og:title" content={seo.title} />}
+            {seo.description && (
+              <meta property="og:description" content={seo.description} />
+            )}
+            {/* {seo.image && <meta property="og:image" content={seo.image} />} */}
+            <meta name="twitter:card" content="summary_large_image" />
+            {seo.title && <meta name="twitter:title" content={seo.title} />}
+            {seo.description && (
+              <meta name="twitter:description" content={seo.description} />
+            )}
+            {/* {seo.image && <meta name="twitter:image" content={seo.image} />} */}
+          </Helmet>
+        </>
       )
     }}
   />
 )
-
-export default SEO
 
 SEO.propTypes = {
   title: PropTypes.string,
@@ -76,3 +76,5 @@ const query = graphql`
     }
   }
 `
+
+export default SEO
