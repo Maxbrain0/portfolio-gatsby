f47d696685e83d8dc9615955986cc7377e453a26
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-07-18 09:00:01 -0700

    layout can be functional component

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,15 +4,11 @@ import "../styles/main.scss"
 import "../util/fontAwesome"
 import Navigation from "./navigation"
 
-class Layout extends React.Component {
-  render() {
-    return (
-      <div className="main">
-        <Navigation className="navigation" />
-        <div className="content">{this.props.children}</div>
-      </div>
-    )
-  }
-}
+const layout = props => (
+  <div className="main">
+    <Navigation className="navigation" />
+    <div className="content">{props.children}</div>
+  </div>
+)
 
-export default Layout
+export default layout
