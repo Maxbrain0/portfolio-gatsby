cba7eaa74c235846b418a650f20f329c142acbd8
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-07-18 12:29:29 -0700

    Add icons to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,9 @@
 import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 import SEO from "../components/SEO"
 import Layout from "../components/layout"
+import styles from "./contact.module.scss"
 
 export default () => (
   <Layout>
@@ -12,6 +14,12 @@ export default () => (
     />
     <div>
       <h1>Contact Me</h1>
+      <div className={styles.contact}>
+        <FontAwesomeIcon icon={["fas", "envelope"]} size="2x" color="#002d5b" />
+        <FontAwesomeIcon icon={["fab", "github"]} size="2x" color="#6e5494" />
+        <FontAwesomeIcon icon={["fab", "linkedin"]} size="2x" color="#0077b5" />
+        <FontAwesomeIcon icon={["fab", "twitter"]} size="2x" color="#1da1f2" />
+      </div>
     </div>
   </Layout>
 )
--- src/util/fontAwesome.js
+++ src/util/fontAwesome.js
@@ -5,7 +5,19 @@ import { library } from "@fortawesome/fontawesome-svg-core"
 import {
   faExternalLinkAlt,
   faPhotoVideo,
+  faEnvelope,
 } from "@fortawesome/free-solid-svg-icons"
-import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import {
+  faGithub,
+  faLinkedin,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons"
 
-library.add(faExternalLinkAlt, faGithub, faPhotoVideo)
+library.add(
+  faExternalLinkAlt,
+  faGithub,
+  faPhotoVideo,
+  faEnvelope,
+  faLinkedin,
+  faTwitter
+)
